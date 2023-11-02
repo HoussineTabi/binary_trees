91c4b19e420d5ce0af4c2b4cf21623621c396db1
HoussineTabi <0620036304ht@gmail.com>
2023-11-02 16:24:11 +0000

    tasks_fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,13 +13,13 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *node = NULL;
 
 	node = malloc(sizeof(binary_tree_t));
-	if (!node)
+	if (!node || !parent)
 		return (NULL);
 	node->parent = parent;
 	node->left = NULL;
 	node->right = NULL;
 	node->n = value;
-	if (parent && parent->left == NULL)
+	if (parent->left == NULL)
 		parent->left = node;
 	else
 	{
