167150a48e4891e857efef68e7396450c969f4c8
HoussineTabi <0620036304ht@gmail.com>
2023-11-02 00:53:00 +0000

    tasks5

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - print a bynary tree in preorder
+ *
+ * @tree: pointer to the root of the tree
+ * @func: pointer to a function to call for each node
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree)
+	{
+		(*func)(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
+}
