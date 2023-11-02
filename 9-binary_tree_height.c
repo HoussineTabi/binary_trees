dfa828f17818617a8db8e18596e78ee313bd0556
HoussineTabi <0620036304ht@gmail.com>
2023-11-02 13:24:30 +0000

    tasks_added

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,6 +10,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height = 0, right_height = 0;
 
+	if (!tree)
+		return (0);
 	if (!tree->left && !tree->right)
 		return (0);
 	if (tree->left)
